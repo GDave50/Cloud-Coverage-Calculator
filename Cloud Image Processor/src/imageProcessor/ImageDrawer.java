ce169a1fb0d0fa8f55a4e27490d194e549048477
Gage Davidson <gaged97@gmail.com>
2017-10-03 12:17:21 -0400

    New source
    Updated program without the initial dialog regarding a full image file. The static final boolean READ_URLS must be changed in order to activate the URL-reading portion of the program. Now, the program will launch directly into the image-sorting portion.

---
+++ Cloud Image Processor/src/imageProcessor/ImageDrawer.java
@@ -0,0 +1,45 @@
+package imageProcessor;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+/**
+ * A JPanel which draws an image as its only content.
+ * @author Gage Davidson
+ */
+class ImageDrawer extends JPanel {
+    
+    /**
+     * Tracks the image that is drawn.
+     */
+    private BufferedImage image;
+    
+    /**
+     * Non-instantiable.
+     */
+    ImageDrawer() {
+    }
+    
+    /**
+     * Paints the panel with the image.
+     */
+    @Override
+    protected synchronized void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        if (image == null)
+            return;
+        
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+    }
+    
+    /**
+     * Sets the image that the panel draws.
+     * @param image new image to draw
+     */
+    synchronized void setImage(BufferedImage image) {
+        this.image = image;
+    }
+}
