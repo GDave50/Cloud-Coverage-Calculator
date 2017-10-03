462e13f63ee17ad2fedcdaa2bba8528d0284d865
Gage Davidson <gaged97@gmail.com>
2017-10-03 12:15:02 -0400

    New source
    Updated program without the initial dialog regarding a full image file. The static final boolean READ_URLS must be changed in order to activate the URL-reading portion of the program. Now, the program will launch directly into the image-sorting portion.

---
+++ src/imageProcessor/Cloud.java
@@ -0,0 +1,23 @@
+package imageProcessor;
+
+/**
+ * Enumerates various cloud types.
+ * @author Gage Davidson
+ */
+enum Cloud {
+    
+    /** Cirrus. */
+    CIRRUS,
+    
+    /** Cumulus. */
+    CUMULUS,
+    
+    /** Other or unknown. */
+    OTHER,
+    
+    /** Stratus. */
+    STRATUS,
+    
+    /** Waterspout. */
+    WATERSPOUT
+}
